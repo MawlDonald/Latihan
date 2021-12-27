0c6bd9d368f3f5cfc92cd8c40032312a40aa277c
MawlDonald <kaitoduluxe0088@gmail.com>
2021-12-27 21:32:07 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Vilain</title>
+
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+
+    <body>      
+        <div class="utama">
+            <div class="header">
+                <h3 class="judul">VILAIN</h3>
+                    <nav>
+                        <a href="index.php" class="menu-home"><b>Home</b></a> 
+                        <a href="kategori.php" class="menu-kategori"><b>Kategori</b></a> 
+                        <a href="specialedition.php" class="menu-special"><b>Special Edition</b></a>
+                    </nav>
+            </div>
+
+            <div class=malasngoding-slider>
+		        <div class=isi-slider>
+			        <img src="img/bg1.jpg" alt="Gambar 1">
+			        <img src="img/bg2.jpg" alt="Gambar 2">
+			        <img src="img/bg3.jpg" alt="Gambar 3">
+                    <img src="img/bg4.jpg" alt="Gambar 4">
+		        </div>
+	        </div>
+
+            <div class="content">
+                <h2 class="h2-content">Selamat datang di VILAIN<h2>
+                    <p align="center"><font color="white">Selling personalized clothes with special designs and limited edition</font></p>
+            </div>
+        </div>
+    </body>
+</html>
